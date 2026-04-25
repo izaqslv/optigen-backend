f42550fa6d289ef41034980c90f6162dd19630e2
Izaq S <izaqslv@gmail.com>
2026-04-25 13:02:46 -0300

    fix: update main

--- api_layer/main_bkp.py
+++ api_layer/main_bkp.py
@@ -1,17 +1,14 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
 import matplotlib
 matplotlib.use("Agg")
-
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api_layer.routes import health_routes, profiles_routes, metadata_routes, auth_routes
+from data.database import engine
+from data.models import Base, User
 from api_layer.routes import users_routes
 from api_layer.routes.analysis_routes import router as analysis_router
 from api_layer.routes.predict_V3_routes import router as V3_router
 from api_layer.routes.simulate_V3_routes import router as simulate_V3_routes
-
-from data.database import engine, Base
-from data import models  # 🔥 ESSENCIAL
 
 app = FastAPI(
     title="OptiGen Intelligence Service",
@@ -19,24 +16,17 @@ app = FastAPI(
     description="Industrial AI Platform - Modular Architecture"
 )
 
-# Middlewares
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-
-# 🔥 CRIAR TABELAS
-Base.metadata.create_all(bind=engine)
-
-# Routers
+# 🔥 Routers
 app.include_router(health_routes.router)
 app.include_router(profiles_routes.router)
 app.include_router(metadata_routes.router)
 app.include_router(auth_routes.router)
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+Base.metadata.create_all(bind=engine)
 app.include_router(users_routes.router, prefix="/users", tags=["users"])
+# app.include_router(analysis_router)
 app.include_router(V3_router)
 app.include_router(simulate_V3_routes)
+
+
 
--- data/models.py
+++ data/models.py
@@ -4,13 +4,12 @@ import uuid
 from data.database import Base
 from datetime import datetime
 
-created_at = Column(DateTime, default=datetime.utcnow)
 
 class User(Base):
     __tablename__ = "users"
 
-    # id = Column(Integer, primary_key=True, index=True)
     id = Column(UUID(as_uuid=True), primary_key=True,
                 default=uuid.uuid4)
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
